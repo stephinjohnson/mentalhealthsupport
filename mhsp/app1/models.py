970cbfe5ad26ae62b2a0f5a98f73266e1dc9d068
Stephin Johnson <stephinjohnsonk@gmail.com>
2023-10-01 23:27:29 +0530

    oct_1

--- mhsp/app1/models.py
+++ mhsp/app1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TherapistProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
