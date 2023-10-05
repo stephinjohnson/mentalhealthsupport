6cd1566fb2adf8493b8f280b3013fa8faffbe73a
Stephin Johnson <stephinjohnsonk@gmail.com>
2023-10-06 00:34:23 +0530

    admincustom

--- mhsp/app1/urls.py
+++ mhsp/app1/urls.py
@@ -7,17 +7,16 @@ from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,Pa
 urlpatterns=[
 
     path('signup/',views.signup,name='signup'),
-    path('',views.login,name='login'),
+    path('', views.user_login, name='login'),
     path('home/',views.home,name='home'),
     path('logout/',views.logoutPage,name='logout'),
     path('thrpreg/',views.ThreapistReg,name='thrpreg'),
-    path('demo/',views.TherapHome,name='demo'),
+    path('custom_admin_page/',views.custom_admin_page,name='custom_admin_page'),
     path('edit/',views.EditProfile,name='edit'),
-    
-    
     path('password_reset', PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 
 ]
