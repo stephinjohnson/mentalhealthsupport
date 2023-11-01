4eeb01af618745e469d1c65b71c5c7d3c4dc8483
Stephin Johnson <stephinjohnsonk@gmail.com>
2023-11-01 23:59:55 +0530

    new functionalities

--- mhsp/app1/models.py
+++ mhsp/app1/models.py
@@ -1,14 +1,29 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
 class User(AbstractUser):
     class Role(models.TextChoices):
-        ADMIN="ADMIN",'Admin'
-        THERAPIST="THERAPIST",'Therapist'
-        USER="USER",'User'
+        ADMIN = "ADMIN", "Admin"
+        THERAPIST = "THERAPIST", "Therapist"
+        USER = "USER", "User"
 
+    role = models.CharField(max_length=50, choices=Role.choices)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
 
-    role = models.CharField(max_length=50,choices=Role.choices)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
