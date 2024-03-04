5a928414889e83e1377c388e75f0abadcf3429fb
Stephin Johnson <stephinjohnsonk@gmail.com>
2024-03-04 09:34:32 +0530

    confirmation modal

--- mhsp/app1/views.py
+++ mhsp/app1/views.py
@@ -826,32 +826,13 @@ def add_time_slot(request):
 
     return render(request, 'add_time_slot.html')
 
-from django.shortcuts import render, redirect
-from .models import TimeSlot, Appointment
+from django.shortcuts import render
+from .models import TimeSlot
 
 def view_time_slots(request):
-    if request.user.role != 'USER':
-        return redirect('home')
-
-    if request.method == 'POST':
-        time_slot_id = request.POST.get('time_slot_id')
-        time_slot = TimeSlot.objects.get(pk=time_slot_id)
-
-        # Check if the time slot is already booked
-        if not time_slot.is_booked:
-            # Mark the time slot as booked
-            time_slot.is_booked = True
-            time_slot.save()
-
-    # Fetch only available time slots (not booked)
     time_slots = TimeSlot.objects.filter(is_booked=False)
 
-    # Check for approved appointments and mark the associated time slots as booked
-    approved_appointments = Appointment.objects.filter(status='APPROVED', time_slot__in=time_slots)
-    booked_time_slots = set(appointment.time_slot for appointment in approved_appointments)
-
-    return render(request, 'view_time_slots.html', {'time_slots': time_slots, 'booked_time_slots': booked_time_slots})
-
+    return render(request, 'view_time_slots.html', {'time_slots': time_slots})
 
 
 # #new timeslotview
@@ -981,14 +962,20 @@ def book_appointment(request, time_slot_id):
             status='PENDING',
             status_change_notification=True
         )
+
+        # Mark the time slot as booked
+        time_slot.is_booked = True
+        time_slot.save()
+
         messages.success(request, f"Appointment request sent to {time_slot.therapist.username}")
 
-        # Add JavaScript code for confirmation alert
-        return render(request, 'confirmation_page.html', {'therapist_username': time_slot.therapist.username})
+        # Redirect to the 'view_time_slots' page
+        return redirect('view_time_slots')
     else:
         messages.warning(request, f"You already have a pending appointment request with {time_slot.therapist.username}")
 
     return redirect('view_time_slots')
+
 
 
 
