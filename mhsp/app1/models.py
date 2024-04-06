6c876d00f280face9d3ce0001a3176d9aa0b28fa
Stephin Johnson <stephinjohnsonk@gmail.com>
2024-04-06 09:14:56 +0530

    MerchandiseUpdates

--- mhsp/app1/models.py
+++ mhsp/app1/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/') 
 
     def __str__(self):
         return self.name
